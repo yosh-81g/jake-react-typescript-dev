afadf054989626678af945379bb77006cf95c4b4
yosh-81g <76814243+yosh-81g@users.noreply.github.com>
2021-03-15 21:26:57 +0900

    finished

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,4 +1,8 @@
-export const Todo = (props) => {
-  const { title, userid } = props;
-  return <p>{`${title}（ユーザー：${userid}）`}</p>;
+import { VFC } from "react";
+import { TodoType } from "./types/todo";
+
+export const Todo: VFC<Omit<TodoType, "id">> = (props) => {
+  const { title, userId, completed = false } = props;
+  const completeMark = completed ? "[完]" : "[未]";
+  return <p>{`${completeMark} ${title}（ユーザー：${userId}）`}</p>;
 };
